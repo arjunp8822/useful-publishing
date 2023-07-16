58e7431976f0ef5f0bd73ed998bd399f79a2795e
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-07-17 09:46:27 +1000

    added about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import aboutImg from '../assets/about.jpg'
+import './About.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='container about'>
+      <div className="about-text">
+        <h3>Useful Publishing</h3>
+        <p>We are Useful publishing. An independent boutique publishing house and as purveyors of the written word our name conveys our purpose and intent. 
+          Being a small boutique business means we are agile and flexible and quite literally think outside the square. We do things differently and are always prepared to explore opportunities. 
+          So if you are fresh independent thinker as an independent or indie author, a book reviewer or blogger or just a book lover, reach out we would love to hear from you.
+        </p>
+        <button>Contact Us</button>
+      </div>
+      <img src={aboutImg} alt="about useful publishing" />
+    </section>
   )
 }
 
