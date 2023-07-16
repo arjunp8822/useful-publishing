a7063f949f0ec4812955a3a639f06eb464188d79
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-07-16 22:31:50 +1000

    added subscribe form

--- frontend/src/components/Subscibe.jsx
+++ frontend/src/components/Subscibe.jsx
@@ -3,7 +3,18 @@ import './Subscribe.css'
 
 const Subscibe = () => {
   return (
-    <div className='subscribe-container'>Subscibe</div>
+    <div className='subscribe'>
+        <form>
+            <h3>Subscribe</h3>
+            <p>Sign up to receive Useful Publishing's monthly bulletin straight to your inbox</p>
+            <div className="input-container">
+                <input type="text" placeholder='First Name'/>
+                <input type="text" placeholder='Last Name'/>
+                <input type="email" placeholder='Email Address'/>
+            </div>
+            <button>Sign Up</button>
+        </form>
+    </div>
   )
 }
 
